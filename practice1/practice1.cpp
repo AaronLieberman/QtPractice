d7242a57f93db414576520a182471e9edac4bf54
Aaron Lieberman <aaronlcode@gmail.com>
2021-10-21 08:47:08 -0700

    add layout

--- practice1/practice1.cpp
+++ practice1/practice1.cpp
@@ -1,12 +1,26 @@
 #include <QApplication>
+#include <QPushButton>
 #include <QTextEdit>
+#include <QVBoxLayout>
+#include <QWidget>
 #include <QtGui>
 
 int main(int argv, char** args) {
 	QApplication app(argv, args);
-	QTextEdit textEdit;
 
-	textEdit.show();
+	QTextEdit* textEdit = new QTextEdit();
+	QPushButton* quitButton = new QPushButton("&Quit");
+
+	QObject::connect(quitButton, SIGNAL(clicked()), &app, SLOT(quit()));
+
+	QVBoxLayout* layout = new QVBoxLayout();
+	layout->addWidget(textEdit);
+	layout->addWidget(quitButton);
+
+	QWidget window;
+	window.setLayout(layout);
+
+	window.show();
 
 	return app.exec();
 }
